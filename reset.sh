6e91c17d575127af4f2e911382b1b4e29d36a9cb
Filgy <filgy@sniff.cz>
2012-02-29 11:16:14 +0100

    Created reset script to download untouched version of test data

---
+++ reset.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "[*] Downloading test data"
+bash -c "curl http://www.fit.vutbr.cz/study/courses/IOS/public/Lab/projekt1/tree.tar | tar xf -"  > /dev/null 2>&1
+mv tree/tests .
+rm -rf tree && mv tests tree
+
+echo "[*] Successfully downloaded"
